af885ad6e8e0150d11de41719e27a08a806b85de
Syahri Rhamadhan <2001020038@student.umrah.ac.id>
2022-12-08 19:28:19 +0700

    tes

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize";
  
-const db = new Sequelize('railway', 'root', '7TRLbLmVJWeSmy5LOfYN', {
-    host: "containers-us-west-131.railway.app",
-    port: "6972",
+const db = new Sequelize('railway', 'root', 'y97ajOE1sRWjE9fjJfMG', {
+    host: "containers-us-west-159.railway.app",
+    port: "5601",
     dialect: "mysql"
 });
  
